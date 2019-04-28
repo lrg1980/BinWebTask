36deb0d9c061b3f552f18d950b5eb77ebc425995
lrg1980 <admin@binweb.net>
2019-04-28 16:13:22 -0300

    Sección Autenticación de usuario con cerrar sesion

--- routes/index.js
+++ routes/index.js
@@ -10,36 +10,55 @@ const authController = require('../controllers/authController')
 
 module.exports = function () {
   // ruta generales
-  router.get('/', proyectosController.proyectosInicio)
+  router.get('/',
+    authController.usuarioAutenticado,
+    proyectosController.proyectosInicio)
+
   // Ruta a página Nuevo Proyecto
-  router.get('/nuevo-proyecto', proyectosController.formularioProyecto)
+  router.get('/nuevo-proyecto',
+    authController.usuarioAutenticado,
+    proyectosController.formularioProyecto)
   router.post('/nuevo-proyecto',
+    authController.usuarioAutenticado,
     // Chequeo de nombre en campo formulario
     body('nombre').not().isEmpty().trim().escape(),
     proyectosController.nuevoProyecto)
-     
+ 
   // Listar proyectos
-  router.get('/proyectos/:url', proyectosController.proyectoPorUrl)
+  router.get('/proyectos/:url',
+    authController.usuarioAutenticado,
+    proyectosController.proyectoPorUrl)
 
   // Actualizar proyecto
-  router.get('/proyecto/editar/:id', proyectosController.formularioEditar)
+  router.get('/proyecto/editar/:id',
+    authController.usuarioAutenticado,
+    proyectosController.formularioEditar)
   // Guardar el nombre nuevo del proyecto
   router.post('/nuevo-proyecto/:id',
+    authController.usuarioAutenticado,
     // Chequeo de nombre en campo formulario
     body('nombre').not().isEmpty().trim().escape(),
     proyectosController.actualizarProyecto)
 
   // Eliminar Proyecto
-  router.delete('/proyectos/:url', proyectosController.eliminarProyecto)
+  router.delete('/proyectos/:url',
+    authController.usuarioAutenticado,
+    proyectosController.eliminarProyecto)
 
   // Tareas
-  router.post('/proyectos/:url', tareasController.agregarTarea)
+  router.post('/proyectos/:url',
+    authController.usuarioAutenticado,
+    tareasController.agregarTarea)
 
   // Actualizar Tarea
-  router.patch('/tareas/:id', tareasController.actualizarTarea)
+  router.patch('/tareas/:id',
+    authController.usuarioAutenticado,
+    tareasController.actualizarTarea)
 
   // Eliminar Tarea
-  router.delete('/tareas/:id', tareasController.eliminarTarea)
+  router.delete('/tareas/:id',
+    authController.usuarioAutenticado,
+    tareasController.eliminarTarea)
 
   // Crear nueva cuenta
   router.get('/crear-cuenta', usuariosController.formCrearCuenta)
@@ -48,6 +67,9 @@ module.exports = function () {
   // Iniciar sesion
   router.get('/iniciar-sesion', usuariosController.formIniciarSesion)
   router.post('/iniciar-sesion', authController.autenticarUsuario)
+
+  // Cerrar sesion
+  router.get('/cerrar-sesion', authController.cerrarSesion)
 
   return router
 }
