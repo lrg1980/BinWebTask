06927d1566fc285e4b37addabdd6eae70666cdcb
lrg1980 <admin@binweb.net>
2019-04-28 21:27:27 -0300

    FIN Proyecto - Todo ok con Localhost

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,4 +1,5 @@
 const Usuarios = require('../models/Usuarios')
+const enviarEmail = require('../handlers/email')
 
 exports.formCrearCuenta = (req, res) => {
   res.render('crearCuenta', {
@@ -24,6 +25,24 @@ exports.crearCuenta = async (req, res) => {
       email,
       password
     })
+
+    // crear una URL de confirmación
+    const confirmarUrl = `http://${req.headers.host}/confirmar/${email}`
+
+    // crear el objeto de usuario
+    const usuario = {
+      email
+    }
+
+    // enviar email
+    await enviarEmail.enviar({
+      usuario,
+      subject: 'Confirma tu cuenta BinwebTask ✔',
+      confirmarUrl,
+      archivo: 'confirmar-cuenta'
+    })
+    // Redirigir al usuario
+    res.flash('correcto', 'Enviamos un correo para confirmar tu cuenta')
     res.redirect('/iniciar-sesion')
   } catch (error) {
     req.flash('error', error.errors.map(error => error.message))
@@ -40,4 +59,24 @@ exports.formResetPassword = (req, res) => {
   res.render('reestablecer', {
     nombrePagina: 'Reestablecer tu contraseña'
   })
+}
+
+exports.confirmarCuenta = async (req, res) => {
+  const usuario = await Usuarios.findOne({
+    where: {
+      email: req.params.correo
+    }
+  })
+
+  // si no existe el usuario
+  if (!usuario) {
+    req.flash('error', 'No válido')
+    res.redirect('/crear-cuenta')
+  }
+
+  usuario.activo = 1
+  await usuario.save()
+
+  req.flash('correcto', 'Cuenta activada correctamente')
+  res.redirect('/iniciar-sesion')
 }
--- routes/index.js
+++ routes/index.js
@@ -63,6 +63,7 @@ module.exports = function () {
   // Crear nueva cuenta
   router.get('/crear-cuenta', usuariosController.formCrearCuenta)
   router.post('/crear-cuenta', usuariosController.crearCuenta)
+  router.get('/confirmar/:correo', usuariosController.confirmarCuenta)
 
   // Iniciar sesion
   router.get('/iniciar-sesion', usuariosController.formIniciarSesion)
